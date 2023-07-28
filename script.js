db9ed30b42f93c5d9615b6fbb1287f6c888eca01
nermline <138861559+nermline@users.noreply.github.com>
2023-07-28 17:45:31 +0300

    pigGame

--- script.js
+++ script.js
@@ -1,58 +1,90 @@
 'use strict';
-let diap = Number(
-  prompt('Введіть діапазон цифр для відгадування (максимально можливе число):')
-);
-document.querySelector('.diap').textContent = diap;
-let secretNum = Math.trunc(Math.random() * diap) + 1;
-let userScore = Number(prompt('Введіть максимальну кількість спроб:'));
-let score = userScore;
-let highScore = 0;
 
-const displayGuessMessage = function (target) {
-  document.querySelector('.guess-message').textContent = target;
+// Elements selection
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Game initial conditions
+let totalScores, currentScore, activePlayer, isPlaying;
+const initGame = function () {
+  score0Element.textContent = 0;
+  score1Element.textContent = 0;
+  current0Element.textContent = 0;
+  current1Element.textContent = 0;
+  totalScores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  isPlaying = true;
+  player0Element.classList.remove('player--winner');
+  player1Element.classList.remove('player--winner');
+  player0Element.classList.remove('player--active');
+  player1Element.classList.remove('player--active');
+  player0Element.classList.add('player--active');
+  diceElement.classList.add('hidden');
 };
+initGame();
 
-document.querySelector('.score').textContent = score;
+function switchPlayer() {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+}
+// Roll the dice
+btnRoll.addEventListener('click', function () {
+  if (isPlaying) {
+    // 1. Generate a random number
+    const diceNumber = Math.trunc(Math.random() * 6) + 1;
 
-document.querySelector('.again').addEventListener('click', function () {
-  document.body.classList.remove('game-win');
-  document.body.classList.remove('game-over');
-  secretNum = Math.trunc(Math.random() * diap) + 1;
-  score = userScore;
-  document.querySelector('.score').textContent = score;
-  // document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
-  document.querySelector('.question').textContent = '???';
-  document.querySelector('.question').style.width = '25rem';
-  displayGuessMessage('Починай вгадувати!');
-  document.querySelector('.number-input').value = '';
-});
+    // 2. Display number on the dice
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice${diceNumber}.png`;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const inputNum = Number(document.querySelector('.number-input').value);
-  if (!inputNum) {
-    displayGuessMessage('Вибери число!');
-  } else if (inputNum === secretNum) {
-    displayGuessMessage('Правильно!');
-    // document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
-    document.body.classList.add('game-win');
-    document.querySelector('.question').style.width = '50rem';
-    document.querySelector('.question').textContent = secretNum;
-    if (score > highScore) {
-      highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
-    }
-  } else if (inputNum !== secretNum) {
-    if (score > 1) {
-      displayGuessMessage(
-        inputNum > secretNum ? 'Занадто багато!' : 'Занадто мало!'
-      );
-      score--;
-      document.querySelector('.score').textContent = score;
+    // 3. If the number is 1, switch to the next player, else add number to the curent score
+
+    if (diceNumber !== 1) {
+      currentScore += diceNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-      displayGuessMessage('Гру закінчено!');
-      document.body.classList.add('game-over');
-      document.querySelector('.score').textContent = 0;
-      // document.querySelector('body').style.backgroundColor = '#ff0000';
+      switchPlayer();
     }
   }
 });
+// Hold button
+btnHold.addEventListener('click', function () {
+  if (isPlaying) {
+    // Add current score to active player total score;
+    totalScores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      totalScores[activePlayer];
+    // If total score >= 100, active player wins, else switch player;
+    if (totalScores[activePlayer] >= 100) {
+      isPlaying = false;
+      diceElement.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceElement.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', initGame);
